825c76d0e5c5abf8838d881addf1f5f14157f307
hyg2010 <81759800+hyg2010@users.noreply.github.com>
2022-05-03 17:06:18 -0500

    update

--- udagram/udagram-frontend/e2e/protractor.conf.js
+++ udagram/udagram-frontend/e2e/protractor.conf.js
@@ -12,6 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
+  baseUrl: 'http://localhost:8080/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
