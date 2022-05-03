a0cb5c42028af5ab0b72894069753d69dc48d9ce
hyg2010 <81759800+hyg2010@users.noreply.github.com>
2022-05-02 23:14:32 -0500

    update

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -6,10 +6,14 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT),
+  token_secret: process.env.TOKEN_SECRET,
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
 };
