1f5855c9906cd111fedd5c545e39f63dde3c2f80
hyg2010 <81759800+hyg2010@users.noreply.github.com>
2022-05-05 00:23:47 -0500

    update

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: 'Udagrambackend-env.eba-sciapwpg.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://Udagrambackend-env.eba-sciapwpg.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
